197887443790940c0da7a633e79b6aeeb0287ce8
v-vova-v <v.makarchuk.v@gmail.com>
2018-07-25 14:18:59 +0300

    minor changes in .spec for test passing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,12 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
-      ],
+      ]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
@@ -22,6 +23,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ngBootcamp!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
   }));
 });
